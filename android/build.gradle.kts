2640ed0c8dee77e9e79776851c142fa45fd101b7
Adil57 <70896717+Adil57@users.noreply.github.com>
2025-12-10 15:30:46 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,17 +1,55 @@
 import java.io.FileInputStream
 import java.util.Properties
 
-// Load local.properties if exists
+// Load local.properties
 val localProperties = Properties()
 val localPropertiesFile = rootProject.file("local.properties")
 if (localPropertiesFile.exists()) {
     localPropertiesFile.inputStream().use { localProperties.load(it) }
 }
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+
+        // Mapbox Maven repo
+        maven("https://api.mapbox.com/downloads/v2/releases/maven") {
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = "mapbox"
+                password = System.getenv("MAPBOX_DOWNLOADS_TOKEN")
+                    ?: localProperties.getProperty("MAPBOX_DOWNLOADS_TOKEN")
+                    ?: ""
+            }
+        }
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
+
+        // Mapbox repo
+        maven("https://api.mapbox.com/downloads/v2/releases/maven") {
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                username = "mapbox"
+                password = System.getenv("MAPBOX_DOWNLOADS_TOKEN")
+                    ?: localProperties.getProperty("MAPBOX_DOWNLOADS_TOKEN")
+                    ?: ""
+            }
+        }
     }
 }
 
